cdd935e35db9acc2af4368cf81a9e59c46028ae5
SergioGMunoz <sergiogmunozdev@gmail.com>
2025-08-08 18:09:57 +0200

    HTML-files

--- js/typing.js
+++ js/typing.js
@@ -7,7 +7,7 @@ if (!text || text.trim() === "") {
   window.location.href = "../index.html";
 }
 
-const cursorPosition = 0;
+let cursorPosition = 0;
 
 // Load the text into de UI
 const renderText = () => {
@@ -16,16 +16,14 @@ const renderText = () => {
 
   document.getElementById("textBefore").innerHTML = textBefore;
   document.getElementById("textAfter").innerHTML = textAfter;
-  changeBgColor();
 };
 
 // Returns if is the correct character
 const checkChar = (char) => {
-    return char === text[cursor];
+    return char === text[cursorPosition];
 }
 
 // Errors management
-
 let errors = 0;
 const errorsCount = document.getElementById('errorsCount');
 
@@ -37,18 +35,42 @@ const error = () => {
 const changeBgColor = (succeed) => {
   const divText = document.getElementById('divText');
   if (succeed) {
-    divText.classList.remove('bg-black');
-    divText.classList.add('bg-red-900');
-  } else {
+    console.log("Changin to black")
     divText.classList.remove('bg-red-900');
     divText.classList.add('bg-black');
+  } else {
+    divText.classList.remove('bg-black');
+    divText.classList.add('bg-red-900');
   }
 };
 
 const renderErrors = () => errorsCount.innerHTML = errors;
 
+// Succeed management
+const succeed = () => {
+  changeBgColor(true);
+  cursorPosition++;
+  renderText();
+}
+
+// Keyboard management 
+document.addEventListener("keydown", (event) => {
+  const char = event.key;
+  console.log('key pressed: ' + char)
+
+  // Ignore special keys like Shift, Control, etc.
+  if (char.length !== 1) return;
+
+  if (checkChar(char)) {
+    succeed();
+  } else {
+    error();
+    renderErrors();
+  }
+});
+
 
 // Render all
 renderText();
 renderErrors();
-changeBgColor(false);
+changeBgColor(true);
